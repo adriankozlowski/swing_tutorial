47748f146bde02ee62331453e62cc90d7ba43b04
akozlowski <adrian.kozlowski@outlook.com>
2017-07-14 20:44:58 +0200

    przyjmowanie połączeń w wątku

--- src/main/java/pl/sda/standalone/serverapp/ServerClass.java
+++ src/main/java/pl/sda/standalone/serverapp/ServerClass.java
@@ -5,6 +5,17 @@
  */
 package pl.sda.standalone.serverapp;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Adrian
@@ -12,7 +23,37 @@ package pl.sda.standalone.serverapp;
 public class ServerClass {
     
     public static void main(String[] args) {
-        
+        try {
+            ServerSocket server = new ServerSocket(10000);
+            Socket accept = server.accept();
+            MyThread thread = new MyThread(accept);
+            thread.start();
+        } catch (IOException ex) {
+            Logger.getLogger(ServerClass.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
+    
+}
+
+class MyThread extends Thread{
+
+    private Socket client;
+    
+    public MyThread(Socket socket) {
+        this.client = socket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            OutputStream outputStream = this.client.getOutputStream();
+            InputStream inputStream = this.client.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            
+        } catch (IOException ex) {
+            Logger.getLogger(MyThread.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
     
 }
