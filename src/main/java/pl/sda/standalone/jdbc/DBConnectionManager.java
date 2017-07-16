9f5a9f3812aab7aa6a2592607498635396d4590b
akozlowski <adrian.kozlowski@outlook.com>
2017-07-16 21:36:44 +0200

    dodany dependency do bazy danych

---
+++ src/main/java/pl/sda/standalone/jdbc/DBConnectionManager.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.sda.standalone.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Adrian
+ */
+public class DBConnectionManager {
+    private Connection connection;
+
+    public DBConnectionManager(String dbURL, String user, String pwd) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        this.connection = DriverManager.getConnection(dbURL, user, pwd);
+    }
+
+    public Connection getConnection() {
+        return this.connection;
+    }
+}
